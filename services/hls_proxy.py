03debfea628114639d7ab639007983ea258d1a1e
nzo66 <nzo66.business@gmail.com>
2025-12-03 20:34:12 +0100

    hls_proxy.py aktualisieren

--- services/hls_proxy.py
+++ services/hls_proxy.py
@@ -122,7 +122,7 @@ class HLSProxy:
                         self.extractors[key] = VavooExtractor(request_headers, proxies=proxies)
                     return self.extractors[key]
                 
-                elif host in ["dlhd", "daddylive", "daddyhd"]:
+                elif host in ["dlhd", "daddylive", "daddyhd", "newkso", "giokko"]:
                     key = "dlhd"
                     proxies = DLHD_PROXIES or GLOBAL_PROXIES
                     if key not in self.extractors:
